107828dd3f717e6b736035754024eae8dfd7e5f5
simonnyman <202305077@post.au.dk>
2024-02-05 11:54:08 +0100

    so far so good

---
+++ test.py
@@ -0,0 +1,8 @@
+from os.path import expanduser
+import datetime
+
+
+file = open(expanduser("~") + '/Desktop/HERE.txt', 'w')
+file.write("It worked!\n" + str(datetime.datetime.now()))
+file.close()
+
