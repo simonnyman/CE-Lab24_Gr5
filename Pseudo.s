0d9bb1af5b0725051e3c0290b61d69e95aaee0c2
simonnyman <202305077@post.au.dk>
2024-05-17 09:52:43 +0200

    pseudo

---
+++ Pseudo.s
@@ -0,0 +1,18 @@
+if obstacle in center angels
+    if obstacle is close
+        smooth turn
+    elif obstacle is too close  
+        u-turn
+elif obstacle in middle angles
+    if obstacle is close
+        smooth turn
+    elif obstacle is too close  
+        hard turn
+elif obstacle in wide angles
+    if obstacle is close
+        smooth turn
+    elif obstacle is too close  
+        medium turn
+else
+    full speed forward
+
